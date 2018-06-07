50e1bd9e8a3f801b6437d5b4a0068bafc6ceb264
Lerespect <pappillon91@live.fr>
2018-06-07 21:02:00 +0200

    comit inutile car je sais pas pouruqoi c'est la enft '-'

--- objet.lua
+++ objet.lua
@@ -1,6 +1,4 @@
 function obdraw()
-  love.graphics.setColor(255, 32, 80)
-  love.graphics.rectangle("fill", 300, 300, 50, 5)
   love.graphics.setColor(255, 32, 20)
   love.graphics.rectangle("fill", 300, 300, 150, 250)
 endlove.graphics.setColor(200, 32, 200)
