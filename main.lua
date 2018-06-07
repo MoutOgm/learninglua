ff99ce5481965b45d9e651e154cbd03a0951ff13
phinat29 <philnat29@orange.fr>
2018-06-07 08:59:10 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+function love.load() --function to start code variables etc
+ -- in code we have space to see better the code
+ -- start wih a variable it's simple
+ variable = 0
+ -- we have 3 types of variables
+ -- number, "string" and true/false
+ -- for exemple variable is a number but try with string
+ variable_string = "it's a string"
+
+end
